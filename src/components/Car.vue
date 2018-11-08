f8c74245219657c73db7c3b952217b6a1c543cbc
Dmitrij Jazel <dj@balticpartner.lt>
2018-11-08 18:52:03 +0200

    Added removeCar() method

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -4,20 +4,33 @@
         <img src="https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet" onerror="this.src='https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet';" :alt="this.car.brand+' '+this.car.brand" :title="this.car.brand+' '+this.car.brand" class="carImg" />
       </td>
 
-      <td>{{this.car._id}}</td>
-      <td>{{this.car.brand}}</td>
-      <td>{{this.car.model}}</td>
+      <td>{{car._id}}</td>
+      <td>{{car.brand}}</td>
+      <td>{{car.model}}</td>
       
-      <td>{{this.car.engine.toFixed(1)}}</td>
-      <td>{{this.car.price.toFixed(2)}} €</td>
+      <td>{{car.engine.toFixed(1)}}</td>
+      <td>{{car.price.toFixed(2)}} €</td>
       
-      <td></td>
+      <td>
+        <button class="car-remove fas fa-trash-alt btn  btn-danger" 
+        @click="removeCar()"></button>
+        </td>
     </tr>
 </template>
 
 <script>
 export default {
   props:['car'],
-  name: 'Car',    
+  name: 'Car',  
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    removeCar: function(){
+      console.log(`remove ${this.car._id}`);
+    }
+  }
 }
 </script>
