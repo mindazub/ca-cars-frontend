aa7004fc96a237cdb7dddf891e5d8d845570c298
Dmitrij Jazel <dj@balticpartner.lt>
2018-11-08 18:25:54 +0200

    Reading data from Car component

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,9 +1,23 @@
 <template>
-    <tr><td colspan="7">Car goes here...</td></tr>
+    <tr>
+      <td>
+        <img src="https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet" onerror="this.src='https://via.placeholder.com/250x125/d2d2d2/?text=no%20image%20yet';" :alt="this.car.brand+' '+this.car.brand" :title="this.car.brand+' '+this.car.brand" class="carImg" />
+      </td>
+
+      <td>{{this.car._id}}</td>
+      <td>{{this.car.brand}}</td>
+      <td>{{this.car.model}}</td>
+      
+      <td>{{this.car.engine}}</td>
+      <td>{{this.car.price}}</td>
+      
+      <td></td>
+    </tr>
 </template>
 
 <script>
 export default {
-  name: 'Car',  
+  props:['car'],
+  name: 'Car',    
 }
 </script>
