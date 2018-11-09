7d8a131417546378276d81b72db123d7d746e9b8
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2018-11-09 18:56:37 +0200

    search form  - filter form plus home element tweaked

---
+++ src/components/HomeElements/FilterForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="mainSearchForm">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Car search</h5>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Brand</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option selected value="" disabled>-Select brand-</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col">
+
+                    </div>
+                </div>
+
+                <a href="#" class="btn btn-primary">Filter</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props:['filterData'], // filters
+        name: 'FilterForm',
+        data(){
+            return {
+            }
+        },
+        methods: {
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="home">
-    <section id="intro">
-          <div class="overVideo">
-        </div>    
-          <video autoplay muted loop id="carsVideo">
-              <source src="@/assets/carsbg.mp4" type="video/mp4">
-              Your browser does not support HTML5 video.
-          </video>    
-    </section>
-
-    <section id="results" class="pt-5">            
+    <div class="home">
+        <section id="intro">
             <div class="container">
-                    <h1>Search car</h1>
+                <filterForm :filterData="filterData"></filterForm>
+            </div>
+
+
+            <div class="overVideo">
+            </div>
+            <video autoplay muted loop id="carsVideo">
+                <source src="@/assets/carsbg.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+            </video>
+        </section>
+
+        <section id="results" class="pt-5">
+            <div class="container">
+                <h1>Search car</h1>
                 <div class="row">
                     <div class="col-12">
                         <table class="table">
                             <thead>
-                                <tr>
-                                    <th scope="col"></th>
-                                    <th scope="col">Brand</th>
-                                    <th scope="col">Model</th>
-                                    <th scope="col">Engine</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Action</th>
-                                </tr>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Brand</th>
+                                <th scope="col">Model</th>
+                                <th scope="col">Engine</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Action</th>
+                            </tr>
                             </thead>
                             <tbody id="tbody_cars">
-                              <car v-for="c in cars" :car="c" :CARS="cars" :key="c._id"></car>
+                            <car v-for="c in cars" :car="c" :CARS="cars" :key="c._id"></car>
                             </tbody>
                         </table>
                     </div>
@@ -34,65 +39,74 @@
             </div>
         </section>
 
-  </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
+    // @ is an alias to /src
 
-import car from '@/components/Car.vue'
+    import car from '@/components/Car.vue'
+    import filterForm from '@/components/HomeElements/FilterForm.vue'
 
-export default {
-  components: {
-        car
-  },
-  data(){
-    return{
-      
-      cars: []
+    export default {
+        components: {
+            car, filterForm
+        },
+        data(){
+            return{
+                filterData: {
+
+                },
+                cars: []
+            }
+        },
+        created(){
+            fetch("http://localhost:5001/api/cars").then(response => response.json()).then(json=>{
+                console.log(json);
+                this.cars = json;
+            })
+        },
+        methods:{
+            printMessage: function(msg){
+                console.log(msg);
+            }
+        }
     }
-  },
-  created(){
-    fetch("http://localhost:5001/api/cars").then(response => response.json()).then(json=>{
-      console.log(json);
-      this.cars = json;
-    })
-  },
-  methods:{
-    printMessage: function(msg){
-      
-    }
-  }
-}
 </script>
 
-<style lang="scss">
-#carsVideo{
-    width:100%;
-}
+<style scoped lang="scss">
+    #carsVideo{
+        width:100%;
+    }
 
-#intro{
-    position: relative;
+    #intro{
+        position: relative;
 
-    overflow: hidden;
-}
+        overflow: hidden;
+    }
 
-#carsVideo{
-    position: absolute;
-    top:-105px;
-    z-index: 45;
-    
-}
+    #carsVideo{
+        position: absolute;
+        top:-105px;
+        z-index: 45;
 
-.overVideo{
-    height:300px;
-    background: orange;
-    opacity: 0.25;
-    overflow-x: hidden;
-    z-index: 50;
-}
+    }
 
-.pageHeader{
-      //background: url("./assets/header_bg.png");
-}
+    .overVideo{
+        height:300px;
+        background: orange;
+        opacity: 0.25;
+        overflow-x: hidden;
+        z-index: 50;
+    }
+
+    .pageHeader{
+        //background: url("./assets/header_bg.png");
+    }
+
+    #mainSearchForm{
+        position: relative;
+        z-index:1000;
+        padding-top:60px;
+    }
 </style>
